b24599bff80969549fb9291259fd02509db99dbb
Josh Hadik <josh.hadik@gmail.com>
2020-03-22 20:01:22 -0500

    setup initial logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+    end
+  end
+
+  scope "api/v1", defaults: {format: :json} do
+    devise_for :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
