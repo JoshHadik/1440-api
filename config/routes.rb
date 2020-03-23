7e3690c601ed864c07d330864981a39c692e479e
Josh Hadik <josh.hadik@gmail.com>
2020-03-22 20:03:53 -0500

    small change

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-    end
-  end
+
 
   scope "api/v1", defaults: {format: :json} do
     devise_for :users
+  end
+
+  namespace :api do
+    namespace :v1 do
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
